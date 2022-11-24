b8da45a8194c2223258ac51132ee3b7fa41f0c3c
Sparrsa <113009586+Sparrsa@users.noreply.github.com>
2022-11-24 22:59:54 +0100

    Add files via upload
    Mycket js

--- Project-Memory-game/app.js
+++ Project-Memory-game/app.js
@@ -1,20 +1,8 @@
 //Declaring our variables.
 const deckContainer = document.querySelector(".deck-container");
-const cardContainer = document.querySelector(".card-container");
+
 let btnStart = document.querySelector(".btn-start");
 let score = document.querySelector("span");
-
-let playerOne = {
-  //Spelare 1
-  name: " ",
-  score: 0,
-};
-
-let playerTwo = {
-  //Spelare 2
-  name: " ",
-  score: 0,
-};
 
 //Generating alla bilder
 const getData = () => [
@@ -68,30 +56,28 @@ const cardGenerator = () => {
   function createCard(imgSrc, name) {
     //skapar element
     let cardContainer = document.createElement("div"); // container till varje par
-    let cardFront = document.createElement("div"); // framsida på kort
-    let cardBack = document.createElement("div"); // baksida på kort
-    let cardImg = document.createElement("img"); //bild till både fram och baksida
-    let cardBackImg = document.createElement("img");
+    // let cardFront = document.createElement("div"); // framsida på kort
+    // let cardBack = document.createElement("div"); // baksida på kort
+    let frontImg = document.createElement("img"); //bild till både fram och baksida
+    let backImg = document.createElement("img");
 
     // ger classer
     cardContainer.setAttribute("class", "card-container");
-    cardContainer.setAttribute("name", name);
-    cardFront.setAttribute("class", "card-front"); //front-face
-    cardBack.setAttribute("class", "card-back"); //back-face
-    cardImg.setAttribute("class", "card-img");
-    cardImg.setAttribute("src", imgSrc);
-
-    cardBackImg.setAttribute("class", "card-back");
-    cardBackImg.setAttribute("src", "img/pink_donut.png");
+    cardContainer.setAttribute("data-name", name);
+    frontImg.setAttribute("class", "card-front"); //front-face
+    backImg.setAttribute("class", "card-back"); //back-face
+    frontImg.setAttribute("src", imgSrc);
+    backImg.setAttribute("src", "img/pink_donut.png");
 
     // lägger ihop element
     deckContainer.append(cardContainer);
-    cardContainer.append(cardFront, cardBack);
-    cardFront.append(cardImg);
-    cardBack.append(cardBackImg);
+    cardContainer.append(frontImg, backImg);
   }
 };
 cardGenerator();
+
+//Väljer ut alla kort. Måste ligga här eftersom allt tidigare är att skapa korten
+const cards = document.querySelectorAll(".card-container");
 
 //if-else statements
 
@@ -101,29 +87,92 @@ cardGenerator();
 //   return score;
 // });
 
-let cardOne;
-let cardTwo;
+let playerOne = {
+  //Spelare 1
+  name: " ",
+  score: 0,
+};
 
-function hasFlipped(event) {
-  // targets cards
-  let clickedCard = event.target;
-  let clickedCardBack = clickedCard.querySelector(".card-back");
+let playerTwo = {
+  //Spelare 2
+  name: " ",
+  score: 0,
+};
 
-  if (clickedCard != cardOne) {
-    clickedCardBack.style.zindex = "1";
-    if (!cardOne) {
-      return (cardOne = clickedCard);
-    }
-    cardTwo = clickedCard;
-  }
-  matchCards();
+//////////////////////////////////////////////
+let players = [playerOne, playerTwo];
+let gameTurn = 0;
+
+const playerTurnLbl = document.querySelector(".player-turn-lbl");
+
+function updateDisplay() {
+  let currentPlayer = players[gameTurn];
+  playerTurnLbl.innerText = currentPlayer.name;
 }
 
-function matchCards() {
-  // kollar om targeted cards matchar
+function startGame() {
+  gameTurn = 0;
+
+  let currentPlayer = players[gameTurn];
+  playerTurnLbl.innerText = currentPlayer.name;
 }
 
 //Generate if/else statements
+
+let hasFlippedCard = false;
+let firstCard;
+let secondCard;
+let cardLock = false;
+
+// Card flip
+function flipCard() {
+  //////////////////////////////////////
+  if (cardLock) return; //Låser möjligheten att flippa fler kort om man redan valt 2st kort
+  if (this === firstCard) return; //Åtgärdar en bugg ifall man klickar för snabbt
+  //Genererar en ny "flip" class till htmlen i card-container varje gång man trycker på ett kort
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    //First card. "Om ett kort inte har vänts och sedan vänds så blir den true"
+    //bredvid .card-container får man ett till class namn som heter ".flip"
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    //second card
+    hasFlippedCard = false;
+    secondCard = this;
+
+    matchedCards(); //skickar vidare till matchedCards funktionen
+  }
+}
+
+function matchedCards() {
+  //Om kort ett och två matchar och har samma "data.name" så kommer addEventListener sluta gälla
+  //för dem korten. Alltså de kommer inte vändas tillbaka.
+  if (firstCard.dataset.name === secondCard.dataset.name) {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+  } else {
+    cardLock = true; // "true" = Alla andra kort förutom dem två valda korten blir "låsta"
+    // Om korten INTE stämmer med varandra så kommer "flipCard" funktionen sluta gälla
+    // och ".flip" classen kommer tas bort från korten
+    setTimeout(() => {
+      firstCard.classList.remove("flip");
+      secondCard.classList.remove("flip");
+      resetCards();
+    }, 2000); //2s timer
+  }
+}
+
+function resetCards() {
+  //tar bort "låsningen" om
+  firstCard = null; // gör så första valalternativet blir null
+  secondCard = null; // gör så andra valalternativet blir null
+  hasFlippedCard = false; // resettar tillbaka korten eftersom inget kort längre är flippat
+  cardLock = false; // Gör låsningen false, alltså tar bort låset
+}
+//Lägger till en event listener på alla kort som lyssnar efter funktionen flipCard
+cards.forEach((card) => card.addEventListener("click", flipCard));
 
 //Reset the game.
 function reset() {
